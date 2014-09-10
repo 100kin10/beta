9411594b0708c5d2fae0a6727c5a9a7065fd009b
Adam Nerland <adam@adamnerland.com>
2014-09-10 13:27:55 -0700

    Adding basic sharing functionality to header links

--- js/100kin10.js
+++ js/100kin10.js
@@ -60,6 +60,12 @@ function responsivequery() {
   }
 };
 
+function fbs_click($shareurl,$sharetitle) {
+	u=$shareurl;
+	window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u),'sharer','toolbar=0,status=0,width=626,height=436');
+	return false;
+};
+
 
 $(document).ready(function () {
 
